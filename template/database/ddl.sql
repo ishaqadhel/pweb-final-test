fffa68d795fa9abcbe9c3d3d3a1c024c6e54689f
ishaqadhel <ishaq.adhel@gmail.com>
2021-12-24 15:18:38 +0700

    feat(template): add mysql dml and ddl script

---
+++ template/database/ddl.sql
@@ -0,0 +1,27 @@
+CREATE TABLE users (
+    id_user int NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    role_position VARCHAR(255),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+
+    CONSTRAINT PK_Users PRIMARY KEY (id_user)
+); 
+
+CREATE TABLE invoice (
+    id_invoice int NOT NULL AUTO_INCREMENT,
+    id_user int,
+    name VARCHAR(255) NOT NULL,
+    semester int NOT NULL,
+    total int NOT NULL,
+    status VARCHAR(255),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+
+    CONSTRAINT PK_Invoice PRIMARY KEY (id_invoice),
+    CONSTRAINT FK_InvoiceParent FOREIGN KEY (id_user)
+    REFERENCES users(id_user)
+); 
---
+++ template/database/dml.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (email, password, name, role, created_at)
+VALUES ('admin@adminsekolah.net', '$2y$10$VgtRVE/wpcc9yUcXN/ZBOu5Nh9NmNcwgXPQu.4KK6f8mXDv0JaLae', 'admin', 'admin', NOW()); 
